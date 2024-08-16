418ed3579f213a8ac637f135b5821d8318c7c3ba
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-16 12:16:53 -0300

    Atualização

--- src/controller/Ex1.java
+++ src/controller/Ex1.java
@@ -1,2 +1,19 @@
-package controller;public class Ex1 {
+package controller;
+
+public class Ex1 {
+    public Ex1(){
+        super();
+    }
+    public int freccontadigito(int n, int qtd){
+        //O método de parada foi pensado que se o dígito for menor que 0, eu já dividi todos os números por 10.
+        if(n < 1){
+            return qtd;
+        }else{
+            //Criei a variável qtd para contar quantas vezes o número digitado foi dividido por 10.
+            qtd++;
+            //A chamada da função recursiva fica dividindo o valor inserido pelo usuário por 10 até cair no ponto de parada.
+            return freccontadigito(n / 10, qtd);
+        }
+    }
+
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,16 @@
+package view;
+
+import controller.Ex1;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex1 m = new Ex1();
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+        int quantidade = 0;
+        int resp = m.freccontadigito(num, quantidade);
+        System.out.println("A quantidade de dígitos é: " + resp);
+
     }
 }
